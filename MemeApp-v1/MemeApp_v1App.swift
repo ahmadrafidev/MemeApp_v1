c9cb3a87bd6636cf89bf0084b63683ea6dcd5dc3
Ahmad Rafi Wirana <ahmad.rafi02@ui.ac.id>
2022-05-23 21:17:49 +0700

    Initial Commit

---
+++ MemeApp-v1/MemeApp_v1App.swift
@@ -0,0 +1,17 @@
+//
+//  MemeApp_v1App.swift
+//  MemeApp-v1
+//
+//  Created by Ahmad Rafi Wirana on 23/05/22.
+//
+
+import SwiftUI
+
+@main
+struct MemeApp_v1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
